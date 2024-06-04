13bc1109954c07c0384392a05c03ac43c445fdb4
zvezdac1f <dhehdgus1231@naver.com>
2024-06-04 19:55:54 +0900

    덧셈식 출력하기_C++

---
+++ 181837.cpp
@@ -0,0 +1,13 @@
+//덧셈식 출력하기
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << a  <<  " + " << b << " = " << a+b << endl;
+    return 0;
+}
