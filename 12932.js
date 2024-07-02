359f1c32a7a27f8b9f8817912949918c00302287
zvezdac1f <dhehdgus1231@naver.com>
2024-07-02 14:23:02 +0900

    자연수 뒤집어 배열로 만들기

---
+++ 12932.js
@@ -0,0 +1,16 @@
+/**자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 
+ * 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다. */
+
+function solution(n) {
+    //먼저 문자를 숫자열로
+    const str = n.toString();
+    //배열을 공백으로 쪼개주고
+    const arr = str.split('')
+    //배열을 거꾸로 해준다음
+    const reverseArr = arr.reverse();
+    //거꾸로해준배열을 숫자로 취해준다
+    const result = reverseArr.map(Number);
+    //그 후 반환
+    return result;
+
+}
--- 12935.js
+++ 12935.js
@@ -3,18 +3,16 @@
  * 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요.
  *  예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다. */
 
-function solution(arr) {
-    //배열이 비어있으면 [-1] 반환
-    if (arr.length === 0) {
-        return [-1];
+function solution(arr){
+    if(arr.length===0){
+        return [-1]
     }
-    //배열의 모든 원소에서 가장 작은 값 찾기
     const min = Math.min(...arr);
-    //배열의 가장 작은 인덱스를 찾아라.
     const idx = arr.indexOf(min);
-    //splice 사용법 (인덱스,제거할 수)
-    arr.splice(idx, 1);
-    if (arr.length === 0) {
+
+    arr.splice(idx,1)
+
+    if(arr.length===0){
         return [-1]
     }
     return arr;
