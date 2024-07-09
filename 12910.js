dc694ebc8dd4de4b6c6fbef08594bcdc8e46ff2a
zvezdac1f <dhehdgus1231@naver.com>
2024-07-09 13:37:37 +0900

    나누어 떨어진는 숫자 배열

--- 12910.js
+++ 12910.js
@@ -4,13 +4,10 @@ divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을
 
 
 function solution(arr, divisor) {
-
-    let answer = arr.filter(e => e % divisor === 0);
-    if (answer.length === 0) {
-        answer = [-1]
+    let answer = arr.filter(e=>e%divisor===0);
+    if(answer.length===0){
+        return [-1]
     }
-
-    answer.sort((a, b) => a - b);
-
-    return answer;
+    answer.sort((a,b)=>a-b)
+    return answer
 }
